7af6540c5370edf6079479830c6cfb993eaeca8f
atbee <atb@odds.team>
2021-06-16 11:03:09 +0700

    Create POST API :bee:

--- main.go
+++ main.go
@@ -7,13 +7,24 @@ import (
 
 const PORT = ":8000"
 
-func hello(w http.ResponseWriter, r *http.Request) {
+func helloWorld(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, World!"))
+}
+
+func helloSomeone(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Create a new ..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", hello)
+	mux.HandleFunc("/hello-world", helloWorld)
+	mux.HandleFunc("/hello-someone", helloSomeone)
 
 	log.Println("Starting server on", PORT)
 	log.Fatal(http.ListenAndServe(PORT, mux))
